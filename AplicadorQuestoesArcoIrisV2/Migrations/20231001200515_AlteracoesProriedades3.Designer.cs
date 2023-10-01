eec58f455589569923acc7efd11ad04c10085dfd
Ewerton <Ewerton@DESKTOP-MN9GPG8>
2023-10-01 17:05:30 -0300

    ajuste de migration

--- AplicadorQuestoesArcoIrisV2/Migrations/20231001200515_AlteracoesProriedades3.Designer.cs
+++ AplicadorQuestoesArcoIrisV2/Migrations/20231001200515_AlteracoesProriedades3.Designer.cs
@@ -2,6 +2,7 @@
 using AplicadorQuestoesArcoIrisV2.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AplicadorQuestoesArcoIrisV2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231001200515_AlteracoesProriedades3")]
+    partial class AlteracoesProriedades3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
---
+++ AplicadorQuestoesArcoIrisV2/Migrations/20231001200515_AlteracoesProriedades3.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AplicadorQuestoesArcoIrisV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteracoesProriedades3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alternativas_Perguntas_PerguntaId",
+                table: "Alternativas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alternativas_PerguntaId",
+                table: "Alternativas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuestaoId",
+                table: "Alternativas",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alternativas_QuestaoId",
+                table: "Alternativas",
+                column: "QuestaoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alternativas_Perguntas_QuestaoId",
+                table: "Alternativas",
+                column: "QuestaoId",
+                principalTable: "Perguntas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alternativas_Perguntas_QuestaoId",
+                table: "Alternativas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alternativas_QuestaoId",
+                table: "Alternativas");
+
+            migrationBuilder.DropColumn(
+                name: "QuestaoId",
+                table: "Alternativas");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alternativas_PerguntaId",
+                table: "Alternativas",
+                column: "PerguntaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alternativas_Perguntas_PerguntaId",
+                table: "Alternativas",
+                column: "PerguntaId",
+                principalTable: "Perguntas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
