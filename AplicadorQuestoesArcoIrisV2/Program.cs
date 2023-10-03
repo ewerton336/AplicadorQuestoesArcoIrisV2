9022f664030221b43cc6bfc1e1e710d8c38408a7
Ewerton Guimarães <ewerton.guimaraes@becomex.com.br>
2023-10-03 12:32:03 -0300

    ajuste de exibição de erros

--- AplicadorQuestoesArcoIrisV2/Program.cs
+++ AplicadorQuestoesArcoIrisV2/Program.cs
@@ -24,11 +24,6 @@ var app = builder.Build();
     app.UseHsts();
 }*/
 
-
-app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
